ce98d55c88bada7d5fbbe694ffca7bd16de06b6d
dengyi <dengyi@dengyi.pro>
2020-09-02 15:54:35 +0800

    init

---
+++ App.vue
@@ -0,0 +1,91 @@
+<script>
+	import {
+		mapState,
+		mapMutations
+	} from 'vuex';
+	export default {
+		onLaunch: function() {
+			const updateManager = uni.getUpdateManager()
+			updateManager.onCheckForUpdate(function(res) {
+				// 请求完新版本信息的回调
+				console.log('应用自动更新检测启动');
+			})
+			updateManager.onUpdateReady(function(res) {
+				uni.showModal({
+					title: '更新提示',
+					content: '新版本已经准备好，是否重启应用？',
+					success(res) {
+						if (res.confirm) {
+							// 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+							updateManager.applyUpdate();
+						}
+					}
+				})
+
+			})
+			updateManager.onUpdateFailed(function(res) {
+				console.error('新版本下载失败')
+			})
+
+			//用户登录
+			this.doLogin()
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		},
+		methods: {
+			...mapMutations(['saveToken', 'saveType', 'saveShowType', 'saveAppName']),
+
+			/**
+			 * 查询应用基本信息
+			 */
+			doGetAppInfo() {
+
+			},
+			/**
+			 * 小程序用户登录
+			 */
+			doLogin() {
+				var that = this
+				uni.login({
+					provider: 'weixin',
+					success(res) {
+						that.$u.api.miniLogin({
+							appId: that.appId,
+							code: res.code
+						}).then(res => {
+							console.log('登录后获取的toekn....', res)
+							that.saveToken(res)
+							//获取应用基本信息
+							that.doRequestAppInfo()
+						}).catch(res => {
+							console.error('用户登录失败', res)
+						})
+					}
+				})
+			},
+			doRequestAppInfo() {
+				this.$u.api.appInfo({
+					id: this.appId
+				}).then(res => {
+					console.log('应用信息为：', res)
+					this.saveType(res.type)
+					this.saveShowType(res.showType)
+					this.saveAppName(res.appName)
+				})
+
+			}
+		},
+		computed: {
+			...mapState(['appId'])
+		}
+	}
+</script>
+
+<style lang="scss">
+	/*每个页面公共css */
+	@import "uview-ui/index.scss";
+</style>
---
+++ store/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+	state: {
+		//应用ID，固定死
+		appId: '1300669936771317762',
+		//用户登录后生成的token
+		token: '',
+		//应用类型1小程序2H5
+		type: '',
+		//显示模式1资讯2壁纸3游戏
+		showType: '',
+		//应用名称
+		appName: ''
+	},
+	mutations: {
+		//保存用户登录token
+		saveToken(state, newToken) {
+			state.token = newToken
+		},
+		//保存小程序类型
+		saveType(state, type) {
+			state.type = type
+		},
+		//保存小程序应用类型
+		saveShowType(state, showType) {
+			state.showType = showType
+		},
+
+		saveAppName(state, appName) {
+			state.appName = appName
+		}
+
+
+	}
+})
+
+export default store
